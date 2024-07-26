ccadca3f596cf8a498dba97fbc0a058739d4c722
peppapig450 <nbgenius1@gmail.com>
2024-07-25 18:08:41 -0700

    start writing the resume update script

---
+++ scripts/update_resume_pdf.py
@@ -0,0 +1,7 @@
+import os
+import shutil
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaIoBaseDownload
+from google.oauth2.service_account import Credentials
+
+FILE_ID = "10Moyqy0xP28Wik_K2BO3MUyWfMCTbRtHsnt1oEytiLE"
